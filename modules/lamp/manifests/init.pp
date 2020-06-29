7b0c36eafc5917f4a918c88ea19d9fae72d22b99
robsandifer1958 <66688690+robsandifer1958@users.noreply.github.com>
2020-06-29 12:01:20 -0500

    Update init.pp

--- modules/lamp/manifests/init.pp
+++ modules/lamp/manifests/init.pp
@@ -1,1 +1,41 @@
+class lamp {
+# execute 'yum update'
+exec { 'yum update':                    # exec resource named yum update'
+  command => '/usr/bin/yum update'  # command this resource will run
+}
 
+# install httpd package
+package { 'httpd':
+  require => Exec['yum update'],        # require yum update' before installing
+  ensure => installed,
+}
+
+# ensure httpd service is running
+service { 'httpd':
+  ensure => running,
+}
+
+# install mysql-server package
+package { 'mysql-community-server':
+  require => Exec['yum update'],        # require yum update' before installing
+  ensure => installed,
+}
+
+# ensure mysql service is running
+service { 'mysqld':
+  ensure => running,
+}
+
+# install php5 package
+package { 'php':
+  require => Exec['yum update'],        # require yum update' before installing
+  ensure => installed,
+}
+
+# ensure info.php file exists
+file { '/var/www/html/info.php':
+  ensure => file,
+  content => '<?php  phpinfo(); ?>',    # phpinfo code
+  require => Package['httpd'],        # require 'httpd' package before creating
+}
+}
